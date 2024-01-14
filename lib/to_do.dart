b173276aa096313436688fa572808b726aee9567
RanaHisham18 <hishamrana00@gmail.com>
2024-01-14 15:06:03 +0200

    Add the highlighted text and the button in the body of the to-do screen

--- lib/to_do.dart
+++ lib/to_do.dart
@@ -19,6 +19,46 @@ class To_Do_List extends StatelessWidget {
           )
         ],
       ),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "Do my math homework",
+                style: TextStyle(backgroundColor: Colors.amber),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("Go to school on time",
+                  style: TextStyle(backgroundColor: Colors.amber)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: const Text("Eat healthy food",
+                  style: TextStyle(backgroundColor: Colors.amber)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: const Text("Study for Science Exam",
+                  style: TextStyle(backgroundColor: Colors.green)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(
+                onPressed: () {
+                  print("You can do this");
+                },
+                color: Colors.green,
+                textColor: Colors.white,
+                child: const Text("Motivation Button"),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
